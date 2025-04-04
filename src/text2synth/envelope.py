fixed decay scaling to [0.0,1.0]

src/text2synth/envelope.py
@@ -43,14 +43,10 @@ class AmpEnvelope(torch.nn.Module):
         # hold accounted by (x - (attack + hold))
 
         # decay drops to sustain
-        decay_curve = torch.clamp((x - (attack + hold)) / (decay + 1e-5), 0.0, 1.0)
-        decay_curve = -decay_curve ** self.n_decay * (1.0 - sustain)
-
         decay_curve = torch.clamp((x - (attack + hold)) / (decay + eps), 0.0, 1.0)
-        decay_curve = (torch.exp(-decay_curve * 2.0) - 1) * (1.0 - sustain) # Exponential decay (adjust the decay factor for control)
+        decay_curve = (torch.exp(-decay_curve * 2.0) - 1.0) / -(torch.exp(torch.tensor(-2.0)) - 1.0) * (1 - sustain)
 
         # release drops to 0.0 linearly
-        #release_curve = (x - (1.0 - release)) * sustain
         release_curve = -torch.clamp((x - (1.0 - release)) * sustain / (release), 0.0, 1.0)
 
         # Combine the phases